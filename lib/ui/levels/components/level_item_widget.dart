ac715a69f4f1745887f669753146000aa3dbdae6
erickLuizA <erickluiz69@gmail.com>
2021-02-21 21:15:23 -0300

    level item widget to it's own component

---
+++ lib/ui/levels/components/level_item_widget.dart
@@ -0,0 +1,93 @@
+import 'package:Queszz/domain/entities/category.dart';
+import 'package:Queszz/ui/shared/empty_start_widget.dart';
+import 'package:Queszz/ui/shared/star_widget.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:flutter/material.dart';
+
+import 'package:Queszz/domain/entities/level.dart';
+
+class LevelItemWidget extends StatelessWidget {
+  final Level level;
+  final Category category;
+
+  const LevelItemWidget({
+    Key key,
+    @required this.level,
+    @required this.category,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        if (level.alreadyTried || level.id == 1) {
+          Navigator.of(context).pushNamed(
+            '/questions',
+            arguments: {
+              "level": level,
+              "category": category,
+            },
+          );
+        }
+      },
+      child: Stack(
+        children: [
+          Column(
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: level.stars == 3
+                    ? [
+                        StarWidget(),
+                        StarWidget(),
+                        StarWidget(),
+                      ]
+                    : level.stars == 2
+                        ? [
+                            StarWidget(),
+                            StarWidget(),
+                            EmptyStarWidget(),
+                          ]
+                        : level.stars == 1
+                            ? [
+                                StarWidget(),
+                                EmptyStarWidget(),
+                                EmptyStarWidget(),
+                              ]
+                            : [
+                                EmptyStarWidget(),
+                                EmptyStarWidget(),
+                                EmptyStarWidget(),
+                              ],
+              ),
+              Text(
+                "?",
+                style: TextStyle(
+                  color: Colors.blueGrey,
+                  fontSize: 40,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              Text(
+                "${AppLocalizations.of(context).level} ${level.id}",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                ),
+              ),
+            ],
+          ),
+          if (!level.alreadyTried && level.id != 1)
+            Align(
+              alignment: Alignment.topCenter,
+              child: Icon(
+                Icons.lock,
+                size: MediaQuery.of(context).size.width / 3,
+                color: Color(0XFF9794b3),
+              ),
+            ),
+        ],
+      ),
+    );
+  }
+}
--- lib/ui/levels/levels_screen.dart
+++ lib/ui/levels/levels_screen.dart
@@ -1,11 +1,14 @@
 import 'package:Queszz/domain/usecases/load_levels.dart';
 import 'package:Queszz/presentation/viewmodels/levels_viewmodel.dart';
-import 'package:Queszz/ui/shared/empty_start_widget.dart';
-import 'package:Queszz/ui/shared/star_widget.dart';
+import 'package:Queszz/ui/shared/load_error_widget.dart';
+import 'package:Queszz/ui/shared/loading_widget.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 import 'package:Queszz/domain/entities/category.dart';
 import 'package:provider/provider.dart';
+
+import 'components/level_item_widget.dart';
 
 class LevelsScreen extends StatefulWidget {
   final Category category;
@@ -48,11 +51,16 @@ class _LevelsScreenState extends State<LevelsScreen> {
         child: Column(
           children: [
             SizedBox(height: 20),
-            if (viewModel.uiState == UIState.Loading)
-              Center(
-                child: CircularProgressIndicator(),
+            if (viewModel.uiState == UIState.Loading) LoadingWidget(),
+            if (viewModel.uiState == UIState.Error)
+              LoadErrorWidget(
+                text: AppLocalizations.of(context).errorLevels,
+                retry: () {
+                  viewModel.loadLevels(
+                    LoadLevelsParams(widget.category.id),
+                  );
+                },
               ),
-            if (viewModel.uiState == UIState.Error) Text("Error"),
             if (viewModel.uiState == UIState.Success)
               Expanded(
                 child: Padding(
@@ -64,76 +72,9 @@ class _LevelsScreenState extends State<LevelsScreen> {
                       (index) {
                         var level = viewModel.levelList[index];
 
-                        return GestureDetector(
-                          onTap: () {
-                            if (level.alreadyTried || level.id == 1) {
-                              Navigator.of(context).pushNamed(
-                                '/questions',
-                                arguments: {
-                                  "level": level,
-                                  "category": widget.category,
-                                },
-                              );
-                            }
-                          },
-                          child: Stack(
-                            children: [
-                              Column(
-                                children: [
-                                  Row(
-                                    mainAxisAlignment: MainAxisAlignment.center,
-                                    children: level.stars == 3
-                                        ? [
-                                            StarWidget(),
-                                            StarWidget(),
-                                            StarWidget(),
-                                          ]
-                                        : level.stars == 2
-                                            ? [
-                                                StarWidget(),
-                                                StarWidget(),
-                                                EmptyStarWidget(),
-                                              ]
-                                            : level.stars == 1
-                                                ? [
-                                                    StarWidget(),
-                                                    EmptyStarWidget(),
-                                                    EmptyStarWidget(),
-                                                  ]
-                                                : [
-                                                    EmptyStarWidget(),
-                                                    EmptyStarWidget(),
-                                                    EmptyStarWidget(),
-                                                  ],
-                                  ),
-                                  Text(
-                                    "?",
-                                    style: TextStyle(
-                                      color: Colors.blueGrey,
-                                      fontSize: 40,
-                                      fontWeight: FontWeight.bold,
-                                    ),
-                                  ),
-                                  Text(
-                                    "Level ${level.id}",
-                                    style: TextStyle(
-                                      color: Colors.white,
-                                      fontSize: 18,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                              if (!level.alreadyTried && level.id != 1)
-                                Align(
-                                  alignment: Alignment.topCenter,
-                                  child: Icon(
-                                    Icons.lock,
-                                    size: MediaQuery.of(context).size.width / 3,
-                                    color: Color(0XFF9794b3),
-                                  ),
-                                ),
-                            ],
-                          ),
+                        return LevelItemWidget(
+                          category: widget.category,
+                          level: level,
                         );
                       },
                     ),
